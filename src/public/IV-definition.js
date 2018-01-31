a6d10af58eb5d7d1bc8df7bfed35473f81e26b46
Don Denton <don@happycollision.com>
2018-01-30 18:16:34 -0600

    more readable/understandable client code example

--- src/public/IV-definition.js
+++ src/public/IV-definition.js
@@ -1,15 +1,15 @@
-var zone = new IV();
+var myIV = new IV();
 
 // Settings section
 
-zone.settings = {
+myIV.settings = {
     baseVideoUrl: 'https://s3.amazonaws.com/ivxml.chapel/interactivation/santatad/MP4/'
 }
 
 // Variables here
 
 
-zone.variables = {
+myIV.variables = {
     count: 0,
     userName: 'Andrey'
 }
@@ -17,50 +17,47 @@ zone.variables = {
 // First Node Comment Here
 // You can describe what it does
 
-    zone.defineNode('First Node')
-        .playVideo('letskeepdoing.mp4')
-        .nextNode('Second Node');
+myIV.defineNode('First Node')
+    .playVideo('letskeepdoing.mp4')
+    .nextNode('Second Node');
         
 
 // Second Node Comment Here
 // You can describe what it does
 
-    zone.defineNode('Second Node')
-        .playVideo('officeparties.mp4')
-        .addButton({ text: 'First Choice', class: 'red bottom center', onClick: 'Third Node'})
-        .addButton({ text: 'Second Choice', class: 'red bottom center', onClick: 'Fourth Node' });
+myIV.defineNode('Second Node')
+    .playVideo('officeparties.mp4')
+    .addButton({ text: 'First Choice', class: 'red bottom center', onClick: 'Third Node'})
+    .addButton({ text: 'Second Choice', class: 'red bottom center', onClick: 'Fourth Node' });
         
 
 
-// Second Node Comment Here
+// Third Node Comment Here
 // You can describe what it does
 
-    zone.defineNode('Third Node')
-        .if({var:'count', greaterThan: 5})
-            .playVideo('http://test.mp4')
-            .nextNode('thirdNode')
-        .if('count == 12')
-            .playVideo('http://test1.mp4')
-            .nextNode('thirdNode1')
-        .else()
-            .playVideo('http://test2.mp4')
-            .nextNode('thirdNode2')
-        .endIf()
-        .playVideo('timenow.mp4')
-        .nextNode('First Node');
+myIV.defineNode('Third Node')
+    .if({var:'count', greaterThan: 5})
+        .playVideo('http://test.mp4')
+        .nextNode('thirdNode')
+    .if('count == 12')
+        .playVideo('http://test1.mp4')
+        .nextNode('thirdNode1')
+    .else()
+        .playVideo('http://test2.mp4')
+        .nextNode('thirdNode2')
+    .endIf()
+    .playVideo('timenow.mp4')
+    .nextNode('First Node');
         
 
-    // End Second node
+// End Third node
 
-    zone.defineNode('Fourth Node')
-        .playVideo('santalikes.mp4')
-        .nextNode('First Node');
-        
-
-
-        console.log(zone.nodes);
-
-        zone.run('First Node');
+myIV.defineNode('Fourth Node')
+    .playVideo('santalikes.mp4')
+    .nextNode('First Node');
+    
 
 
+console.log(myIV.nodes);
 
+myIV.run('First Node');
