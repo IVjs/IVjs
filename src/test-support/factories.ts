afbbc2ff03a081367c90d45d7278556cfc5a5640
Don Denton <don@happycollision.com>
2018-02-08 15:03:55 -0600

    Adjust factory file for our purpose here

--- src/test-support/factories.ts
+++ src/test-support/factories.ts
@@ -1,49 +1,112 @@
-import assign from 'object-assign';
+class Definitions {
+  getRandomNumberCommand = (): ICommand.GetRandomNumber => ({
+    name: 'getRandomNumber',
+    min: 1,
+    max: 100,
+    assignTo: 'myFavoriteVar',
+  })
 
-const definitions = () => {
-  return {
-    template: {
-      id: getNewId(),
-      tags: [],
-      name: 'Sparky Pro Electrocutionator',
-      type: 'TextOnly',
-      image: 'some/image.jpg',
-      duration: '0:23',
-      plan: 'Expert',
-      price: '$5'
-    },
-    templateGroup: {
-      id: getNewId(),
-      tags: [],
-      name: 'Some Great Group Name',
-      type: 'TextOnly',
-      image: 'some/image.jpg',
-      duration: '0:23',
-      plan: 'Expert',
-      price: '$5',
-      children: []
-    }
-  }
-};
+  playVideoCommand = (): ICommand.PlayVideo => ({
+    name: 'playVideo',
+    file: 'anyvideo.mp4',
+  })
+
+  assignVariableCommand = (): ICommand.AssignVariable => ({
+    name: 'assignVariable',
+    assignTo: 'count',
+    value: 3,
+  })
+
+  assignFromVariableCommand = (): ICommand.AssignFromVariable => ({
+    name: 'assignVariable',
+    assignTo: 'count',
+    varName: 'someVarName',
+  })
+
+  targetCommand = (): ICommand.Target => ({
+    name: 'target',
+    keyName: 'nameOfThisTarget',
+  })
+
+  switchCommand = (): ICommand.Switch => ({
+    name: 'switch',
+    do: [],
+    defaultCommands: [],
+  })
+
+  stopExecutionCommand = (): ICommand.StopExecution => ({
+    name: 'stopExecution',
+  })
+
+  pauseExecutionCommand = (): ICommand.PauseExecution => ({
+    name: 'pauseExecution',
+  })
+
+  goToNodeCommand = (): ICommand.GoToNode => ({
+    name: 'goToNode',
+    nodeName: 'someNodeName',
+  })
+
+  executeAsyncCommand = (): ICommand.ExecuteAsync => ({
+    name: 'executeAsync',
+    nodeName: 'someNodeName',
+  })
+
+  executeSyncCommand = (): ICommand.ExecuteSync => ({
+    name: 'executeSync',
+    nodeName: 'someNodeName',
+  })
+
+  waitCommand = (): ICommand.Wait => ({
+    name: 'wait',
+    time: 1000,
+  })
+
+  timeoutCommand = (): ICommand.Timeout => ({
+    name: 'timeout',
+    time: 1000,
+    commands: [],
+  })
+
+  goToCommand = (): ICommand.GoToCommand => this.goToCommand_usingNode();
+
+  goToCommand_usingNode = (): ICommand.GoToCommand => ({
+    name: 'goToCommand',
+    nodeName: 'someNode',
+  })
+
+  goToCommand_usingTarget = (): ICommand.GoToCommand => ({
+    name: 'goToCommand',
+    target: 'someTarget',
+  })
+
+  calculateCommand = (): ICommand.Calculate => ({
+    name: 'calculate',
+    varName: 'someVarName',
+    operation: 'add',
+    assignTo: 'someOtherVarName',
+  })
+}
+const definitions = new Definitions();
+
 
 let incrementor = 1;
-
 function getNewId() {
   return incrementor++;
 }
 
-function getFaketory(faketory) {
-  const defs = definitions();
-  if (defs[faketory] === undefined) {
+function getFaketory<T extends keyof Definitions>(faketory: T) {
+  if (definitions[faketory] === undefined) {
     throw new Error(`Test factory \`create('${faketory}')\` failed. There is no factory called "${faketory}"`);
   }
-  return definitions()[faketory];
+  return definitions[faketory]();
 }
 
-export function create(faketory, overrides) {
+export function create<T extends keyof Definitions>(faketory: T, overrides) {
   let obj = getFaketory(faketory);
   overrides = overrides || {};
-  return assign({}, obj, overrides);
+  Object.assign(obj, overrides);
+  return obj;
 }
 
 export default create;
