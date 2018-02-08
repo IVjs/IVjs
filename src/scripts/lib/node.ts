9e303a75b9fd8071c5c8937c1353305707181ad8
aarkhipov40 <andrey@flixpress.com>
2018-02-08 13:04:55 -0600

    Clean

--- src/scripts/lib/node.ts
+++ src/scripts/lib/node.ts
@@ -2,26 +2,18 @@ interface VideoOptions {
   file: string;
 }
 
+
 type PlayVideoInput = (string | VideoOptions) | Array<string | VideoOptions>;
 
 export class Node {
   private addingToCondition = false;
 
-  public conditions: any[] = [];
-  public buttons: any[] = [];
-  public url: string = null;
-  public next: string = null;
   private commands: ICommand.AnyCommand[] = [];
 
-  public condition: any = {
-    url: null,
-    next: null,
-    buttons: []
-  }
 
   constructor( public name: string ) { }
 
-  public playVideo(url: PlayVideoInput) {
+  public videoPlay(url: PlayVideoInput) {
     const newInput = [].concat(url)
     newInput.forEach(vs => this.createVideoObj(vs))
     return this;
@@ -45,52 +37,5 @@ export class Node {
       name: 'playVideo',
       file: url
     });
-  }
-
-  public nextNode(node) {
-    if(this.addingToCondition) {
-      this.condition.next = node;
-    } else {
-      this.next = node;
-    }
-    return this;
-  }
-
-  public addButton(buttonObj) {
-    if(this.addingToCondition) {
-      this.condition.buttons.push(buttonObj);
-    } else {
-      this.buttons.push(buttonObj);
-    }
-    return this;
-  }
-
-  if(conditionText) {
-    // if condition needs to be added
-    if(this.addingToCondition) {
-      this.conditions.push(this.condition);
-    } else {
-      this.addingToCondition = true;
-    }
-
-    // reset condition
-    this.condition = {};
-    this.condition.buttons = new Array();
-    this.condition.type = 'if';
-    this.condition.statement = conditionText;
-    return this;
-  }
-
-  else() {
-    this.conditions.push(this.condition);
-    this.condition = new Object();
-    this.condition.type = 'else';
-    return this;
-  }
-
-  endIf() {
-    this.conditions.push(this.condition);
-    this.addingToCondition = false;
-    return this;
   }
 }
