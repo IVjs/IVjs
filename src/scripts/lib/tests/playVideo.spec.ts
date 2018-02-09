da45cf686cab147d0abe02d556404ae0f8b5e643
Don Denton <don@happycollision.com>
2018-02-09 11:06:55 -0600

    rewrite spec for video play to be the correct command name

--- src/scripts/lib/tests/playVideo.spec.ts
+++ src/scripts/lib/tests/playVideo.spec.ts
@@ -6,24 +6,24 @@ describe('playVideo()', () => {
   beforeEach(() => iv = new IV())
 
   describe('when given a string', () => {
-    xtest('it creates a valid video object', () => {
+    test('it creates a valid video object', () => {
       const expectedObject = create('playVideoCommand', {
         url: 'test.mp4',
       });
 
-      iv.node('anything').playVideo('test.mp4');
+      iv.node('anything').videoPlay('test.mp4');
 
       expect(iv.nodes[0].commands[0]).toEqual(expectedObject);
     })
   })
 
   describe('when given an object', () => {
-    xtest('it creates a valid video object', () => {
+    test('it creates a valid video object', () => {
       const expectedObject = create('playVideoCommand', {
         url: 'test.mp4',
       });
 
-      iv.node('anything').playVideo({url:'test.mp4'});
+      iv.node('anything').videoPlay({url:'test.mp4'});
 
       expect(iv.nodes[0].commands[0]).toEqual(expectedObject);
     })
