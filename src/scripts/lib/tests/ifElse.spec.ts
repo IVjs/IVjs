6d2f9f3bee2c63767c61e95a87dfaaf0190508af
aarkhipov40 <andrey@flixpress.com>
2018-02-13 00:15:22 -0600

    More test

--- src/scripts/lib/tests/ifElse.spec.ts
+++ src/scripts/lib/tests/ifElse.spec.ts
@@ -10,14 +10,14 @@ describe('if-else()', () => {
       const expectedObject = {
           name: 'switch',
           do: [
-            {varName: 'count', is: 5, commands:[{name:'goToNode', nodeName:'first'},{name:'stopExecution'}]}
+            {varName: 'count', is: 5, commands:[{name:'playVideo', file:'test.mp4'},{name:'goToNode', nodeName:'first'},{name:'stopExecution'}]}
           ],
           defaultCommands: [{name:'goToNode', nodeName:'second'},{name:'stopExecution'}]
       };
 
       // why would this fill the .nodes[0].commands[0-1] ????.  It should fill the switch command.
 
-      iv.node('anything').if({var:'count', is: 5}).goto('first').else().goto('second').endIf();
+      iv.node('anything').if({var:'count', is: 5}).videoPlay('test.mp4').goto('first').else().goto('second').endIf();
       
       expect(iv.nodes[0].commands[0]).toEqual(expectedObject);
     })
