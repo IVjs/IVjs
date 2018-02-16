2aeec3827fddafec2eec6a46282bf66ff4fe0a1b
Don Denton <don@happycollision.com>
2018-02-16 09:52:50 -0600

    Add test helpers for video needs

--- src/test-support/index.ts
+++ src/test-support/index.ts
@@ -2,6 +2,7 @@ export * from './factories';
 export * from './simulateEvent';
 export * from './dom-commands';
 export * from './mock-classes';
+export * from './integration-helpers'
 
 import wait from './wait';
 export {wait};
