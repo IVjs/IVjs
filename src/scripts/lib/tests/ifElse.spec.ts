c011ef45554959ff68d24191e6e479d97a7b508f
Don Denton <don@happycollision.com>
2018-02-19 17:02:47 -0600

    fixup make if else spec more readable

--- src/scripts/lib/tests/ifElse.spec.ts
+++ src/scripts/lib/tests/ifElse.spec.ts
@@ -7,15 +7,14 @@ describe('if-else()', () => {
 
   describe('when given a condition', () => {
     test('it creates a valid switch Command', () => {
-      const expectedObject = {
+      const expectedObject = create('switchCommand',{
           name: 'switch',
           do: [
             {varName: 'count', is: 5, commands:[{name:'playVideo', file:'test.mp4'},{name:'goToNode', nodeName:'first'},{name:'stopExecution'}]},
             {varName: 'count', is: 6, commands:[{name:'playVideo', file:'test1.mp4'},{name:'goToNode', nodeName:'first1'},{name:'stopExecution'}]}
          ],
           defaultCommands: [{name:'goToNode', nodeName:'second'},{name:'stopExecution'}]
-      };
-      const expectedObject1 = {name:'playVideo', file:'another.mp4'}
+      });
 
       iv.node('anything')
         .if({var:'count', is: 5})
@@ -25,10 +24,8 @@ describe('if-else()', () => {
         .else()
           .goto('second')
         .endIf()
-        .videoPlay('another.mp4');
       
       expect(iv.nodes[0].commands[0]).toEqual(expectedObject);
-      //expect(iv.nodes[0].commands[1]).toEqual(expectedObject1);
     })
   })
 })
