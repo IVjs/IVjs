e09d88fc5cb25b4fd41b42d89f2c2aadcd671fff
Don Denton <don@happycollision.com>
2018-02-20 14:03:06 -0600

    add plenty more operators

--- src/scripts/lib/commandEngine/generalCommands/calculate.spec.ts
+++ src/scripts/lib/commandEngine/generalCommands/calculate.spec.ts
@@ -52,4 +52,142 @@ describe('calculate factory', () => {
       expect(sw.changedVar()).toEqual(1);
     })
   });
+
+  describe('operators: multiply', () => {
+    test('multiplies a given value', () => {
+      const sw = createSimpleCalculateInput({
+        startingValue: 8,
+        operator: 'multiply',
+        operand: 8,
+      })
+
+      doCalculate(sw.given, sw.command)
+
+      expect(sw.changedVar()).toEqual(64);
+    })
+  });
+
+  describe('operators: divide', () => {
+    test('divides given value', () => {
+      const sw = createSimpleCalculateInput({
+        startingValue: 64,
+        operator: 'divide',
+        operand: 8,
+      })
+
+      doCalculate(sw.given, sw.command)
+
+      expect(sw.changedVar()).toEqual(8);
+    })
+  });
+
+  describe('operators: divideThenRemainder', () => {
+    test('divides given value and returns the remainder', () => {
+      const sw = createSimpleCalculateInput({
+        startingValue: 12,
+        operator: 'divideThenRemainder',
+        operand: 7,
+      })
+
+      doCalculate(sw.given, sw.command)
+
+      expect(sw.changedVar()).toEqual(5);
+    })
+  });
+
+  describe('operators: divideThenRoundUp', () => {
+    test('divides given value and rounds up', () => {
+      const sw = createSimpleCalculateInput({
+        startingValue: 12,
+        operator: 'divideThenRoundUp',
+        operand: 7,
+      })
+
+      doCalculate(sw.given, sw.command)
+
+      expect(sw.changedVar()).toEqual(2);
+    })
+  });
+
+  describe('operators: divideThenRoundDown', () => {
+    test('divides given value and rounds down', () => {
+      const sw = createSimpleCalculateInput({
+        startingValue: 12,
+        operator: 'divideThenRoundDown',
+        operand: 7,
+      })
+
+      doCalculate(sw.given, sw.command)
+
+      expect(sw.changedVar()).toEqual(1);
+    })
+  });
+
+  describe('operators: divideThenRound', () => {
+    test('rounds up at 0.5', () => {
+      const sw = createSimpleCalculateInput({
+        startingValue: 1,
+        operator: 'divideThenRound',
+        operand: 2,
+      })
+
+      doCalculate(sw.given, sw.command)
+
+      expect(sw.changedVar()).toEqual(1);
+    })
+
+    test('rounds down below 0.5', () => {
+      const sw = createSimpleCalculateInput({
+        startingValue: 1,
+        operator: 'divideThenRound',
+        operand: 2.00001,
+      })
+
+      doCalculate(sw.given, sw.command)
+
+      expect(sw.changedVar()).toEqual(0);
+    })
+  });
+
+  describe('operators: round', () => {
+    test('rounds the value from the variable', () => {
+      const sw = createSimpleCalculateInput({
+        startingValue: 12.5,
+        operator: 'round',
+        operand: 7, // ignored
+      })
+
+      doCalculate(sw.given, sw.command)
+
+      expect(sw.changedVar()).toEqual(13);
+    })
+  });
+
+  describe('operators: roundUp', () => {
+    test('rounds the value up from the variable', () => {
+      const sw = createSimpleCalculateInput({
+        startingValue: 12.2,
+        operator: 'roundUp',
+        operand: 7, // ignored
+      })
+
+      doCalculate(sw.given, sw.command)
+
+      expect(sw.changedVar()).toEqual(13);
+    })
+  });
+
+  describe('operators: roundDown', () => {
+    test('rounds the value down from the variable', () => {
+      const sw = createSimpleCalculateInput({
+        startingValue: 12.7,
+        operator: 'roundDown',
+        operand: 7, // ignored
+      })
+
+      doCalculate(sw.given, sw.command)
+
+      expect(sw.changedVar()).toEqual(12);
+    })
+  });
 });
--- src/scripts/lib/commandEngine/generalCommands/calculate.ts
+++ src/scripts/lib/commandEngine/generalCommands/calculate.ts
@@ -13,14 +13,24 @@ export function doCalculate(
 ): Runner.CommandReturn {
   const { variables } = given;
   const { operation, varName, value, assignTo } = cmd;
+  const startingValue = variables[varName]
   variables[assignTo] = 
-    getOperation(operation)(variables[varName], value)
+    getOperation(operation)(startingValue, value)
   return {};
 }
 
 const operations = {
   add: (val1, val2) => val1 + val2,
   subtract: (val1, val2) => val1 - val2,
+  multiply: (val1, val2) => val1 * val2,
+  divide: (val1, val2) => val1 / val2,
+  divideThenRemainder: (val1, val2) => val1 % val2,
+  divideThenRoundDown: (val1, val2) => Math.floor(val1 / val2),
+  divideThenRoundUp: (val1, val2) => Math.ceil(val1 / val2),
+  divideThenRound: (val1, val2) => Math.round(val1 / val2),
+  round: (val1) => Math.round(val1),
+  roundUp: (val1) => Math.ceil(val1),
+  roundDown: (val1) => Math.floor(val1),
 }
 
 function getOperation(operator: string) {
