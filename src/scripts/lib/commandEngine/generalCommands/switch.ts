4809d296dc0f3af521525555342216ca1209194d
Don Denton <don@happycollision.com>
2018-02-20 10:32:50 -0600

    pass for operator precedence

--- src/scripts/lib/commandEngine/generalCommands/switch.ts
+++ src/scripts/lib/commandEngine/generalCommands/switch.ts
@@ -14,6 +14,7 @@ export function doSwitch(
   const { variables } = given;
   let winningCommand;
   cmd.do.forEach(condition => {
+    if (winningCommand) return;
     const givenVar = variables[condition.varName];
     if (givenVar === condition.is) {
       winningCommand = condition.commands;
