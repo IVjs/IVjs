3e8efed4ec21230b86766cd56621513c8f2742de
Don Denton <don@happycollision.com>
2018-02-20 13:21:40 -0600

    add calculate with addition available

---
+++ src/scripts/lib/commandEngine/generalCommands/calculate.spec.ts
@@ -0,0 +1,41 @@
+import { calculateFactory, doCalculate } from './calculate';
+import { create, createMockEngine } from '../../../../test-support'
+
+function createSimpleCalculateInput({ operator, startingValue, operand }) {
+  const given = create('targetFunctionFactoryInput', { variables: { myVar: startingValue } });
+  const swCmd = create('calculateCommand', {
+    varName: 'myVar',
+    operation: operator,
+    value: operand,
+    assignTo: 'changedVar',
+  });
+
+  return { given, command: swCmd, changedVar: () => given.variables.changedVar };
+}
+
+describe('calculate factory', () => {
+  test('it produces a valid TFO', () => {
+    const tfo = calculateFactory({
+      settings: create('ivSettings'),
+      commandEngine: createMockEngine(),
+      variables: {}
+    });
+
+    expect(tfo).toHaveProperty('calculate')
+    expect(typeof tfo.calculate).toEqual('function')
+  })
+
+  describe('operators: add', () => {
+    test('adds to given value', () => {
+      const sw = createSimpleCalculateInput({
+        startingValue: 12,
+        operator: 'add',
+        operand: 12,
+      })
+
+      doCalculate(sw.given, sw.command)
+
+      expect(sw.changedVar()).toEqual(24);
+    })
+  });
+});
---
+++ src/scripts/lib/commandEngine/generalCommands/calculate.ts
@@ -0,0 +1,31 @@
+export const calculateFactory: CommandEngine.TargetFunctionFactory = (input): Runner.TargetFunctionObject => {
+
+  return {
+    'calculate':
+      (cmd: ICommand.Calculate) =>
+        Promise.resolve(doCalculate(input, cmd))
+  }
+}
+
+export function doCalculate(
+  given: CommandEngine.TargetFunctionFactoryInput,
+  cmd: ICommand.Calculate
+): Runner.CommandReturn {
+  const { variables } = given;
+  const { operation, varName, value, assignTo } = cmd;
+  variables[assignTo] = 
+    getOperation(operation)(variables[varName], value)
+  return {};
+}
+
+const operations = {
+  add: (val1, val2) => val1 + val2,
+}
+
+function getOperation(operator: string) {
+  const theOperation = operations[operator];
+  if (!theOperation) {
+    throw new Error(`There is no "${operator}" operator in the calculate command`);
+  }
+  return theOperation;
+}
