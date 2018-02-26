dccbd0f1044251536086df132784fb48f3f837a8
Don Denton <don@happycollision.com>
2018-02-26 12:09:37 -0600

    test new syntax

--- src/scripts/lib/node-builders/video/video-play.spec.ts
+++ src/scripts/lib/node-builders/video/video-play.spec.ts
@@ -13,7 +13,7 @@ describe('playVideo()', () => {
 
       iv.node('anything').playVideo('test.mp4');
 
-      expect(iv.nodes[0].commands[0]).toEqual(expectedObject);
+      expect(iv.nodes[0].commands).toEqual([expectedObject]);
     })
   })
 
@@ -25,22 +25,66 @@ describe('playVideo()', () => {
 
       iv.node('anything').playVideo({ url: 'test.mp4'});
 
-      expect(iv.nodes[0].commands[0]).toEqual(expectedObject);
+      expect(iv.nodes[0].commands).toEqual([expectedObject]);
     })
+  })
 
-    test('it can play sequentially', () => {
+  test('it can play sequentially', () => {
+    const expectedObject = create('playVideoCommand', {
+      file: 'test.mp4',
+      onComplete: [
+        create('playVideoCommand', {
+          file: 'test2.mp4',
+        })
+      ]
+    });
+
+    iv.node('anything').playVideo('test.mp4', 'test2.mp4');
+
+    expect(iv.nodes[0].commands).toEqual([expectedObject]);
+  })
+
+  describe('option merging', () => {
+    test('it merges options with the previous video', () => {
       const expectedObject = create('playVideoCommand', {
         file: 'test.mp4',
         onComplete: [
+          create('goToNodeCommand', { nodeName: 'someNode' }),
+          create('stopExecutionCommand'),
           create('playVideoCommand', {
             file: 'test2.mp4',
           })
         ]
       });
 
-      iv.node('anything').playVideo('test.mp4', 'test2.mp4');
+      iv.node('anything').playVideo(
+        'test.mp4',
+        { goTo: 'someNode' },
+        'test2.mp4',
+      );
 
-      expect(iv.nodes[0].commands[0]).toEqual(expectedObject);
+      expect(iv.nodes[0].commands).toEqual([expectedObject]);
     })
-  })
+
+    test('it merges options with the previous object', () => {
+      const expectedObject = create('playVideoCommand', {
+        file: 'test.mp4',
+        onComplete: [
+          create('goToNodeCommand', { nodeName: 'someNode' }),
+          create('stopExecutionCommand'),
+          create('playVideoCommand', {
+            file: 'test2.mp4',
+          })
+        ]
+      });
+
+      iv.node('anything').playVideo(
+        { url: 'test.mp4' },
+        { goTo: 'someNode' },
+        'test2.mp4',
+      );
+
+      expect(iv.nodes[0].commands).toEqual([expectedObject]);
+    });
+  });
 })
