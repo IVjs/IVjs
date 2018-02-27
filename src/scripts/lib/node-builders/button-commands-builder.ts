71457593db7967d9a936e5de6a2e78c5e8aa83d2
Don Denton <don@happycollision.com>
2018-02-27 17:22:19 -0600

    buttons now run commands on click

--- src/scripts/lib/node-builders/button-commands-builder.ts
+++ src/scripts/lib/node-builders/button-commands-builder.ts
@@ -14,8 +14,42 @@ export class ButtonCommandsBuilder {
       name: 'addButton',
       id,
       text,
-      onClick: []
+      onClick: this.createCommands(input)
     }
     return cmd;
+  }
+
+  private createCommands(input: ButtonOptions): ICommand.AnyCommand[] {
+    const { runAsync, goTo, js, remove, id } = input;
+    const commands: ICommand.AnyCommand[] = [];
+
+    if (runAsync) {
+      commands.push({
+        name: 'executeAsync',
+        nodeName: runAsync,
+      })
+    }
+    if (js) {
+      commands.push({
+        name: 'executeJs',
+        func: js
+      })
+    }
+    if (remove) {
+      commands.push({
+        name: 'removeButton',
+        id
+      })
+    }
+    if (goTo) {
+      commands.push({
+        name: 'goToNode',
+        nodeName: goTo
+      },{
+        name: 'stopExecution'
+      })
+    }
+
+    return commands;
   }
 }
