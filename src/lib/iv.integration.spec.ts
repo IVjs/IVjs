5f3829e5d2ca406ab2be99653d28a13d3b2e17b0
Don Denton <don@happycollision.com>
2018-11-12 17:33:34 -0600

    Reorg: move js integration test

--- src/lib/iv.integration.spec.ts
+++ src/lib/iv.integration.spec.ts
@@ -24,33 +24,6 @@ describe('integration', () => {
     iv.variables = {};
   })
 
-  describe('.js()', () => {
-    test('it runs the passed function', () => {
-      const spy = jest.fn();
-      iv.node('anything')
-        .js(spy)
-
-      iv.run('anything');
-
-      expect(spy).toHaveBeenCalled();
-    });
-
-    test('it is still chainable', async () => {
-      const spy = jest.fn();
-      const spy2 = jest.fn();
-      iv.node('anything')
-        .js(spy)
-        .js(spy2)
-
-      iv.run('anything');
-
-      await wait();
-
-      expect(spy).toHaveBeenCalled();
-      expect(spy2).toHaveBeenCalled();
-    });
-  })
-
   describe('.if()', () => {
     beforeEach(() => {
       iv.variables = { count: 2 };
---
+++ src/lib/plugins/unserializable-plugins/execute-js/execute-js.integration.spec.ts
@@ -0,0 +1,35 @@
+import { wait } from '../../../../test-support';
+import { IV } from '../../../iv';
+
+describe('.js()', () => {
+  let iv: IV;
+  beforeEach(() => {
+    iv = new IV();
+    iv.variables = {};
+  })
+
+  test('it runs the passed function', () => {
+    const spy = jest.fn();
+    iv.node('anything')
+      .js(spy)
+
+    iv.run('anything');
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test('it is still chainable', async () => {
+    const spy = jest.fn();
+    const spy2 = jest.fn();
+    iv.node('anything')
+      .js(spy)
+      .js(spy2)
+
+    iv.run('anything');
+
+    await wait();
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy2).toHaveBeenCalled();
+  });
+});
