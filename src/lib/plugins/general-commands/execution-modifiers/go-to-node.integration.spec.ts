e7270ae98fc59615a5db3b1c91131e0fb4d1f812
Don Denton <don@happycollision.com>
2018-11-12 17:51:58 -0600

    Create integration spec for goToNode()

---
+++ src/lib/plugins/general-commands/execution-modifiers/go-to-node.integration.spec.ts
@@ -0,0 +1,28 @@
+import { wait } from '../../../../test-support';
+import { IV } from '../../../iv';
+
+describe('.goToNode()', () => {
+  let iv: IV;
+  beforeEach(() => {
+    iv = new IV();
+    iv.variables = { count: 0 };
+  })
+
+  test('pauses execution and resumes when other node completes', async () => {
+    iv.node('first')
+      .calculate({ var: 'count', add: 1 })
+      .goToNode('second')
+
+    iv.node('second')
+      .calculate({ var: 'count', add: 1 })
+      .goToNode('third')
+
+    iv.node('third')
+      .calculate({ var: 'count', add: 1 })
+
+    iv.run('first');
+
+    await wait(6);
+    expect(iv.variables.count).toEqual(3)
+  });
+})
