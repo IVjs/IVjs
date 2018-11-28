f82eff783d369d56a5b84cad43f3293d8c7a8f96
Don Denton <don@happycollision.com>
2018-11-28 14:35:20 -0600

    DragIten: failing test for adding image to DOM

--- src/lib/plugins/dom-commands/drag-and-drop/add-drag-item.spec.ts
+++ src/lib/plugins/dom-commands/drag-and-drop/add-drag-item.spec.ts
@@ -1,8 +1,9 @@
 import { create } from '../../../../test-support/factories';
 import { IV } from '../../../iv';
+import { querySelectorAll } from '../../../../test-support';
 
 describe('addDragItem', () => {
-  let iv;
+  let iv: IV;
   beforeEach(() => (iv = new IV()));
 
   describe('commands', () => {
@@ -11,7 +12,17 @@ describe('addDragItem', () => {
 
       iv.node('anything').addDragItem({ id: 'draggable', image: 'anImage.jpg' });
 
-      expect(iv.nodes[0].commands[0]).toEqual(expectedObject);
+      expect((iv as any).nodes[0].commands[0]).toEqual(expectedObject);
+    });
+  });
+
+  describe('integration', () => {
+    test('it adds the image to the DOM', () => {
+      iv.node('anything').addDragItem({ id: 'draggable', image: 'anImage.jpg' });
+
+      iv.run('anything');
+
+      expect(querySelectorAll('img')).toHaveLength(1);
     });
   });
 });
