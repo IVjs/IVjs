9e670a9cd8c8dc810e8f063926ab212c2daa31bd
Don Denton <don@happycollision.com>
2018-12-04 11:17:33 -0600

    Add message about simulate event not getting an element

--- src/test-support/simulateEvent.ts
+++ src/test-support/simulateEvent.ts
@@ -1,4 +1,7 @@
 export function simulateEventOnElement(name: string, el: HTMLElement, evtData = {}) {
+  if (el == null) {
+    throw new Error('Simulate event received no element');
+  }
   const evt = document.createEvent('HTMLEvents');
   Object.assign(evt, evtData);
   evt.initEvent(name, false, true);
