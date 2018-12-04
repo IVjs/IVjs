c63f8032da6d5c5b25865b3de53125a72f84201c
Don Denton <don@happycollision.com>
2018-12-04 11:17:43 -0600

    fix broken karma tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,33 +1,24 @@
+const tsconf = require('./tsconfig.json');
+const webpackConf = require('./webpack.config');
+delete webpackConf.entry;
+delete webpackConf.output;
+
 module.exports = function(config) {
   config.set({
-    frameworks: ['jasmine', 'karma-typescript'],
+    frameworks: ['jasmine'],
     exclude: ['src/test-support/setup-env.ts'],
-    files: ['src/**/!(*.spec).ts'],
+    files: ['src/**/*.test.ts', 'src/karma-support/*'],
     preprocessors: {
-      'src/**/*.ts': 'karma-typescript', // *.tsx for React Jsx
+      'src/karma-support/*': ['webpack', 'sourcemap'],
+      'src/**/*.test.ts': ['webpack', 'sourcemap'],
     },
-    reporters: ['progress', 'karma-typescript', 'kjhtml'],
+    reporters: ['progress', 'kjhtml'],
     browsers: ['Chrome'],
 
     client: {
       clearContext: false,
     },
 
-    karmaTypescriptConfig: {
-      compilerOptions: {
-        emitDecoratorMetadata: true,
-        experimentalDecorators: true,
-        module: 'commonjs',
-        lib: ['es2015', 'dom'],
-        sourceMap: true,
-        target: 'ES5',
-        types: ['jasmine'],
-      },
-      include: ['src/**/*.ts'],
-      exclude: ['node_modules', 'src/**/*.spec.ts', 'src/test-support/setup-env.ts'],
-      bundlerOptions: {
-        transforms: [require('karma-typescript-es6-transform')()],
-      },
-    },
+    webpack: webpackConf,
   });
 };
