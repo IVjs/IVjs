7432c8f698b169b00d9a86b66f5dd2059b2c7654
Don Denton <don@happycollision.com>
2018-12-05 18:31:34 -0600

    Add button to reveal zones

--- docs/demos/zones.js
+++ docs/demos/zones.js
@@ -6,53 +6,69 @@ myIV.settings = {
 
 myIV.variables = {
   clickedZone: '',
+  help: false,
 }
 
 myIV.node('First Node')
+  .removeAllButtons()
   .playVideo('loop.mp4', { goToNode: 'Wait Loop' })
   .wait(0.5)
+  .runAsync('add zones')
+  .addButton({id: 'need help', text: 'Where sould I click?', goToNode: 'need help', remove: true})
+
+myIV.node('add zones')
   .addZone({
-    visible: true,
+    visible: '{{help}}',
     id: 'cheek',
     left: 42, top: 45,
     width: 5, height: 7,
     onClick: { setVariable: 'clickedZone', goToNode: 'Handle Zones' }
   })
   .addZone({
-    visible: true,
+    visible: '{{help}}',
     id: 'leye',
     left: 43, top: 38,
     width: 5, height: 5,
     onClick: { setVariable: 'clickedZone', goToNode: 'Handle Zones' }
   })
   .addZone({
-    visible: true,
+    visible: '{{help}}',
     id: 'reye',
     left: 51, top: 38,
     width: 5, height: 5,
     onClick: { setVariable: 'clickedZone', goToNode: 'Handle Zones' }
   })
   .addZone({
-    visible: true,
+    visible: '{{help}}',
     id: 'mouth',
     left: 45, top: 52,
     width: 9, height: 7,
     onClick: { setVariable: 'clickedZone', goToNode: 'Handle Zones' }
   })
   .addZone({
-    visible: true,
+    visible: '{{help}}',
     id: 'lshoulder',
     left: 35, top: 58,
     width: 9, height: 7,
     onClick: { setVariable: 'clickedZone', goToNode: 'Handle Zones' }
   })
   .addZone({
-    visible: true,
+    visible: '{{help}}',
     id: 'rshoulder',
     left: 55, top: 58,
     width: 9, height: 7,
     onClick: { setVariable: 'clickedZone', goToNode: 'Handle Zones' }
   })
+
+myIV.node('add help zones')
+  .runSync('remove zones')
+  .setVariable({storeIn: 'help', value: true})
+  .runSync('add zones')
+  .setVariable({storeIn: 'help', value: false}) // This line seems not to work at the moment.
+
+myIV.node('need help')
+  .runSync('add help zones')
+  .goToNode('Wait Loop')
 
 myIV.node('Wait Loop')
   .playVideo('loop.mp4', {goToNode:'Wait Loop'})
